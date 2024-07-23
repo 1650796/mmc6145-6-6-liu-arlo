26c0288b9bfd805e4c224965d7d81c096325c646
catsociety <aliu6876@gmail.com>
2024-07-22 21:59:39 -0400

    all tests pass

--- pages/api/book.js
+++ pages/api/book.js
@@ -5,12 +5,49 @@ import db from '../../db'
 // this handler runs for /api/book with any request method (GET, POST, etc)
 export default withIronSessionApiRoute(
   async function handler(req, res) {
+    if (!req.session.user) {
+      return res.status(401).json({ error: "You are not authorized to access this page." })
+    }
+
+    switch(req.method) {
     // TODO: On a POST request, add a book using db.book.add with request body 
+    case 'POST':
+      try {
+        const book = req.body
+        const addBook = await db.book.add(req.session.user.id, book)
+
+        if (addBook === null) {
+          req.session.destroy()
+          return res.status(401).json({ error: "User not found." })
+        } else {
+          return res.status(200).json("Book added to favorites.")
+        }
+      } catch (err) {
+        return res.status(400).json({ error: err.message })
+      }
+      
     // TODO: On a DELETE request, remove a book using db.book.remove with request body 
+    case 'DELETE':
+      try {
+        const removeBook = await db.book.remove(req.session.user.id, req.body.id)
+
+        if (removeBook === null) {
+          req.session.destroy()
+          return res.status(401).json({ error: "User not found." })
+        } else {
+          return res.status(200).json("Book removed from favorites.")
+        }
+      } catch (err) {
+        return res.status(400).json({ error: err.message })
+      }
+
     // TODO: Respond with 404 for all other requests
-    // User info can be accessed with req.session
-    // No user info on the session means the user is not logged in
-    return res.status(404).end()
+    default:
+      return res.status(404).end()    
+    }
   },
   sessionOptions
 )
+
+    // User info can be accessed with req.session
+    // No user info on the session means the user is not logged in
--- pages/book/[id].jsx
+++ pages/book/[id].jsx
@@ -45,17 +45,39 @@ export default function Book(props) {
       router.push('/')
   }, [props.book, bookSearchResults, book, router])
 
-  async function addToFavorites() {
+  async function addToFavorites(e) {
     // TODO: use fetch to call POST /api/book
     // Be sure to pass book in body (use JSON.stringify)
     // Be sure to also include the content-type header as application/json
+    e.preventDefault()
+    const res = await fetch('/api/book', {
+      method: 'POST',
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(book)
+    })
     // Call router.replace(router.asPath) if you receive a 200 status
+    if (res.status === 200) {
+      router.replace(router.asPath)
+    }
   }
-  async function removeFromFavorites() {
+  async function removeFromFavorites(e) {
     // TODO: use fetch to call DELETE /api/book
     // Be sure to pass {id: <book id>} in body (use JSON.stringify)
     // Be sure to also include the content-type header as application/json
+    e.preventDefault()
+    const res = await fetch('/api/book', {
+      method: 'DELETE',
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({id: book.id})
+    })
     // Call router.replace(router.asPath) if you receive a 200 status
+    if (res.status === 200) {
+      router.replace(router.asPath)
+    }
   }
 
   return (
